f42ab6988974d9248398c46821dad6dfb78f82d0
federicomaffei <federico.maffei@gmail.com>
2014-10-26 16:11:54 +0000

    added socket.io module and connection.

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var express = require("express");
 var app = express();
 var http = require("http");
 var server = http.Server(app);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req,res){
 	res.sendFile('index.html', {"root": __dirname});
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected to socket.');
 });
 
 server.listen('3000', function(){
